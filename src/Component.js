ac4657f6be0ea4005a6adf1fbd45d49e1104c7f2
j-iann <87838923+j-iann@users.noreply.github.com>
2021-11-23 00:38:31 -0300

    enviando arquivos

---
+++ src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Container extends Component {
+  render() {
+    return(
+      <div id="container">
+        <h1>Olá mundo React</h1>
+        <h2>Esse é meu componente de classe :]</h2>
+      </div>
+    );
+  };
+};
+
